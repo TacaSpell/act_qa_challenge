966d560d63bfddbe05e631e9268f88b54e99462c
Yan David <yan.folk@gmail.com>
2024-08-26 03:12:16 -0300

    Covering invalid email scenario for registration

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -12,5 +12,20 @@ describe("Registration feature", () => {
         it('Then an error message is displayed: "Invalid email address"', () => {
             cy.contains("Invalid email address");
         });
+
+        describe("Invalid email", { testIsolation: false }, () => {
+            it("Given I am on the login page", () => {
+                cy.visit(Cypress.env("LOGIN_URL"));
+            });
+            it("When the Email address field is filled with an invalid email", () => {
+                cy.get("#email").type("lorem ipsum");
+            });
+            it('And the "Create an account" button is clicked', () => {
+                cy.get("#SubmitCreate > span").click();
+            });
+            it('Then an error message is displayed: "Invalid email address"', () => {
+                cy.contains("Invalid email address");
+            });
+        });
     });
 });
