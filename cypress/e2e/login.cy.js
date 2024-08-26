d7a5d352a4230f40ca3df1539284f564343546ec
Yan David <yan.folk@gmail.com>
2024-08-26 01:36:11 -0300

    Covering authentication failure scenario for login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker";
+
 describe("Login feature", () => {
     const LOGIN_URL =
         "http://www.automationpractice.pl/index.php?controller=authentication&back=my-account";
@@ -24,6 +26,25 @@ describe("Login feature", () => {
         });
         it('Then an error message is displayed: "Invalid email address"', () => {
             cy.contains("Invalid email address");
+        });
+    });
+    describe("Authentication failure", { testIsolation: false }, () => {
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        it("Given I am on the login page", () => {
+            cy.visit(LOGIN_URL);
+        });
+        it("When the Email address field is filled correctly", () => {
+            cy.get("#email").type(email);
+        });
+        it("and the Password field is filled correctly", () => {
+            cy.get("#passwd").type(password);
+        });
+        it('And the "Sign in" button is clicked', () => {
+            cy.get("#SubmitLogin > span").click();
+        });
+        it('Then an error message is displayed: "Authentication failed"', () => {
+            cy.contains("Authentication failed");
         });
     });
 });
