1ac977abd74df6727d1fa8996d9f8cd37ce6bc17
Yan David <yan.folk@gmail.com>
2024-08-26 01:13:18 -0300

    Covering invalid email scenario for login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,4 +12,18 @@ describe("Login feature", () => {
             cy.contains("An email address required");
         });
     });
+    describe("Invalid email", { testIsolation: false }, () => {
+        it("Given I am on the login page", () => {
+            cy.visit(LOGIN_URL);
+        });
+        it("When the Email address field is filled with an invalid email", () => {
+            cy.get("#email").type("lorem ipsum");
+        });
+        it('And the "Sign in" button is clicked', () => {
+            cy.get("#SubmitLogin > span").click();
+        });
+        it('Then an error message is displayed: "Invalid email address"', () => {
+            cy.contains("Invalid email address");
+        });
+    });
 });
